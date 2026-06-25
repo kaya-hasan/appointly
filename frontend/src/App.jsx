09f3ad793f1a6b3942e684694eea6f19b5782b28
Hasan KAYA <hasankaya3546@hotmail.com>
2026-06-25 22:02:54 +0300

    Clean up frontend app shell imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,17 @@
-import HomePage from "./pages/HomePage.jsx"
-import MainLayout from "./layouts/MainLayout.jsx"
-import CustomersPage from "./pages/CustomersPage.jsx"
-import './App.css'
-import AppointmentsPage from "./pages/AppointmentsPage.jsx"
+import MainLayout from "./layouts/MainLayout.jsx";
+import CustomersPage from "./pages/CustomersPage.jsx";
+import AppointmentsPage from "./pages/AppointmentsPage.jsx";
+import "./App.css";
 
 function App() {
-
   return (
     <div>
       <MainLayout>
-
         <CustomersPage />
         <AppointmentsPage />
       </MainLayout>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
